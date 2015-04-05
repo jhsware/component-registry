c544148206da740a54b58e27aa729f199f37ea5f
jhsware <sebastian@urbantalk.se>
2015-04-05 22:06:47 +0200

    Super working for contructor

--- test/test-objectPrototype.js
+++ test/test-objectPrototype.js
@@ -48,5 +48,89 @@ describe('Object Prototypes', function() {
         expect(user.sayHi()).to.be("Hi!");
         expect(user.sayHo()).to.be("Ho!");
     });
+    
+    it("can inherit from other object prototypes two levels deep", function() {
+        var IUser = createInterface();
+        
+        var userProto = createObjectPrototype({
+            implements: [IUser],
+            sayHi: function () {
+                return "Hi!"
+            }
+        })
+        
+        var ISpecialUser = createInterface();
+        
+        var specialUserProto = createObjectPrototype({
+            extends: [userProto],
+            implements: [ISpecialUser],
+            sayHo: function () {
+                return "Ho!"
+            }
+        })
+        
+        var user = new specialUserProto();
+        
+        var ISuperSpecialUser = createInterface();
+        
+        var superSpecialUserProto = createObjectPrototype({
+            extends: [specialUserProto],
+            implements: [ISuperSpecialUser],
+            sayYay: function () {
+                return "Yay!"
+            }
+        })
+        
+        var user = new superSpecialUserProto();
+        
+        expect(user).to.be.a(superSpecialUserProto);
+        expect(user.sayHi()).to.be("Hi!");
+        expect(user.sayHo()).to.be("Ho!");
+        expect(user.sayYay()).to.be("Yay!");
+    });
+    
+    it("can inherit from other object prototypes two levels deep and call all constructors", function() {
+        var IUser = createInterface();
+        
+        var userProto = createObjectPrototype({
+            implements: [IUser],
+            constructor: function () {
+                this.storedVal = (this.storedVal || '') + '1';
+            },
+            getStoredVal: function () {
+                return this.storedVal;
+            }
+        })
+        
+        var ISpecialUser = createInterface();
+        
+        var specialUserProto = createObjectPrototype({
+            extends: [userProto],
+            implements: [ISpecialUser],
+            constructor: function (data, _super_) {
+                _super_._constructor.call(this, data);
+                this.storedVal = (this.storedVal || '') + '2';
+            },
+        })
+        
+        var user = new specialUserProto();
+        
+        var ISuperSpecialUser = createInterface();
+        
+        var superSpecialUserProto = createObjectPrototype({
+            extends: [specialUserProto],
+            implements: [ISuperSpecialUser],
+            constructor: function (data, _super_) {
+                _super_._constructor.call(this, data);
+                this.storedVal = (this.storedVal || '') + '3';
+            },
+        })
+        
+        var user = new superSpecialUserProto();
+        
+        expect(user).to.be.a(superSpecialUserProto);
+        expect(user.getStoredVal()).to.be("123");
+    });
+    
     it("todo...", function() {});
 });
