e4ffe8c85697ae25c77e0a2a00c778875014e9ce
jhsware <sebastian@urbantalk.se>
2018-09-06 19:14:48 +0200

    Added test for important readme example... (and found a typo)

--- test/test-adapterFactory.js
+++ test/test-adapterFactory.js
@@ -1,10 +1,13 @@
-var assert = require('assert');
 var expect = require('expect.js');
 
 const Interface = require('../lib').createInterfaceClass('test');
-const { Adapter } = require('../lib');
+const { Adapter, globalRegistry, createObjectPrototype } = require('../lib');
 
 describe('Adapter Factory', function() {
+    beforeEach(function () {
+        globalRegistry.adapters = {}
+    })
+
     it('can create an adapter', function() {
         var IUser = new Interface({name: 'IUser'});
         var IDisplayWidget = new Interface({name: 'IDisplayWidget'});
@@ -17,7 +20,7 @@ describe('Adapter Factory', function() {
         expect(adapter).not.to.be(undefined);
     });
 
-    it("checks for existence of members in implemnented interface", function() {
+    it("checks for existence of methods in implemnented interface", function() {
         var IUser = new Interface({name: 'IUser'});
         var IDisplayWidget = new Interface({name: 'IDisplayWidget'});
         
@@ -41,5 +44,30 @@ describe('Adapter Factory', function() {
             failed = true
         }
         expect(failed).to.equal(true);
+    });
+
+    it("make sure we actually can call the methods on the adapter", function() {
+        var IUser = new Interface({name: 'IUser'});
+        var IDisplayWidget = new Interface({name: 'IDisplayWidget'});
+        
+        IDisplayWidget.prototype.render = function () {}
+        
+        var adapter = new Adapter({
+          implements: IDisplayWidget,
+          adapts: IUser,
+          render(inp) {
+              return inp
+          }
+        })
+
+        const User = createObjectPrototype({
+            implements: [IUser]
+        })
+    
+        const theUser = new User()
+
+        const outp = new IDisplayWidget(theUser).render('test')
+
+        expect(outp).to.be('test');
     });
 });
