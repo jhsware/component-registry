3328a0d298d0d73084564235fe6d6cb5281a7d2b
Sebastian Ware <sebastian@urbantalk.se>
2022-12-03 22:54:32 +0100

    fixed most registry tests

--- __tests__/test-registry.ts
+++ __tests__/test-registry.ts
@@ -3,266 +3,291 @@ import { describe, expect, it, beforeEach } from "@jest/globals";
 import {
     globalRegistry as registry,
     LocalRegistry,
-    createInterfaceClass,
-    Adapter,
-    Utility,
-    createObjectPrototype } from '../dist/index.cjs.js'
+    AdapterInterface, createIdFactory, ObjectInterface, TAdapter, TUtility, UtilityInterface, Utility, Adapter, ObjectPrototype
+} from "../src";
 
-const Interface = createInterfaceClass('test')
+const id = createIdFactory('test');
 
-describe('Global Registry', function() {
+describe('Global Registry', function () {
     beforeEach(function () {
         registry.utilities = {}
         registry.adapters = {}
     })
 
-    it('can be created', function() {
+    it('can be created', function () {
         expect(registry).not.toBe(undefined);
     });
 
-    it('can get an unnamed utility', function() {
-        const IDummyUtility = new Interface({name: "IDummyUtility"});
-        
-        const DummyUtility = new Utility({
-            implements: IDummyUtility
-        });
-        
+    it('can get an unnamed utility', function () {
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
+
+        class DummyUtility extends Utility {
+            get __implements__() { return IDummyUtility };
+            constructor({ name, registry }: TUtility) {
+                super({ name, registry });
+            }
+        }
+
+        new DummyUtility({})
+
         const util = new IDummyUtility();
-        
+
         expect(util).toBeInstanceOf(DummyUtility);
     });
-    
-    it('can get a named utility', function() {
-        const IDummyUtility = new Interface({name: "IDummyUtility"});
-        
-        const DummyUtility = new Utility({
-            implements: IDummyUtility,
-            name: 'basic'
-        });
-        
+
+    it('can get a named utility', function () {
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
+
+        class DummyUtility extends Utility {
+            get __implements__() { return IDummyUtility };
+            constructor({ name, registry }: TUtility) {
+                super({ name, registry });
+            }
+        }
+
+        new DummyUtility({ name: 'basic' });
+
         const util = new IDummyUtility('basic');
-        
+
         expect(util).toBeInstanceOf(DummyUtility);
     });
-    
-    it('can get the correct named utility', function() {
-        const IDummyUtility = new Interface({name: "IDummyUtility"});
-        
-        const DummyUtility = new Utility({
-            implements: IDummyUtility,
-            name: 'basic'
-        });
-        
-        const NotMeUtility = new Utility({
-            implements: IDummyUtility,
-            name: 'not me'
-        });
-        
+
+    it('can get the correct named utility', function () {
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
+
+        class DummyUtility extends Utility {
+            get __implements__() { return IDummyUtility };
+            constructor({ name, registry }: TUtility) {
+                super({ name, registry });
+            }
+        }
+
+        const me = new DummyUtility({ name: 'basic' });
+        const notMe = new DummyUtility({ name: 'not me' });
+
         const util = new IDummyUtility('basic');
-        
-        expect(util).toBeInstanceOf(DummyUtility);
-        expect(util).not.toBeInstanceOf(NotMeUtility);
+
+        expect(util).toBe(me);
+        expect(util).not.toBe(notMe);
     });
-    
-    it("returns 'undefined' if named utility isn't found and we have passed true at end", function() {
-        const IDummyUtility = new Interface({name: "IDummyUtility"});
-        
-        const DummyUtility_1 = new Utility({
-            implements: IDummyUtility,
-            name: 'one'
-        });
-        
-        const utils = registry.getUtility(IDummyUtility, 'two', undefined);
-        
-        expect(utils).toBe(undefined);
-    });
-    
-    it('can register adapter with convenience method', function() {
-        const IUser = new Interface({name: "IUser"});
-        
-        const IUserAdapter = new Interface({name: "IUserAdapter"});
-        
-        const UserAdapter = new Adapter({
-            implements: IUserAdapter,
-            adapts: IUser
-        })
-        
-        const UserPrototype = createObjectPrototype({
-            implements: [IUser]
-        });
-        
-        const theUser = new UserPrototype();
-        
+
+    // it("returns 'undefined' if named utility isn't found and we have passed true at end", function() {
+    //             class IDummyUtility extends ObjectInterface {
+    //     get interfaceId() { return id('IDummyUtility') };
+    // }
+
+    //     const DummyUtility_1 = new Utility({
+    //         implements: IDummyUtility,
+    //         name: 'one'
+    //     });
+
+    //     const utils = registry.getUtility(IDummyUtility, 'two', undefined);
+
+    //     expect(utils).toBe(undefined);
+    // });
+
+    it('can register adapter with convenience method', function () {
+        class IUser extends ObjectInterface {
+            get interfaceId() { return id('IUser') };
+        }
+
+
+        class IUserAdapter extends AdapterInterface {
+            get interfaceId() { return id('IUserAdapter') };
+        }
+
+        class UserAdapter extends Adapter {
+            get __implements__() { return IUserAdapter };
+            constructor({ adapts, Component, registry }: TAdapter) {
+                super({ adapts, Component, registry });
+            }
+        }
+        new UserAdapter({ adapts: IUser })
+
+        class User extends ObjectPrototype<any> {
+            __implements__ = [IUser];
+        }
+
+        const theUser = new User();
+
         const ua = new IUserAdapter(theUser);
 
         expect(ua).toBeInstanceOf(UserAdapter);
     });
-    
-    it('can get an adapter registered by interface', function() {
-        const IUser = new Interface({name: "IUser"});
-        
-        const IUserAdapter = new Interface({name: "IUserAdapter"});
-        
-        const UserAdapter = new Adapter({
-            implements: IUserAdapter,
-            adapts: IUser
-        })
-        
-        const UserPrototype = createObjectPrototype({
-            implements: [IUser]
-        });
-        
-        const theUser = new UserPrototype();
-        
+
+    it('can get an adapter registered by interface', function () {
+        class IUser extends ObjectInterface {
+            get interfaceId() { return id('IUser') };
+
+        }
+
+        class IUserAdapter extends AdapterInterface {
+            get interfaceId() { return id('IUserAdapter') };
+        }
+
+        class UserAdapter extends Adapter {
+            get __implements__() { return IUserAdapter };
+            constructor({ adapts, Component, registry }: TAdapter) {
+                super({ adapts, Component, registry });
+            }
+        }
+        new UserAdapter({ adapts: IUser })
+
+        class User extends ObjectPrototype<any> {
+            __implements__ = [IUser];
+          }
+        const theUser = new User();
+
         const ua = new IUserAdapter(theUser);
 
         expect(ua).toBeInstanceOf(UserAdapter);
     });
-    
-    it('can get an adapter by specifying the adaptsInterface param', function() {
-        const IUser = new Interface({name: "IUser"});
-        
-        const IStrong = new Interface({name: "IStrong"});
-        
-        const IUserAdapter = new Interface({name: "IUserAdapter"});
-        
-        const StrongAdapter = new Adapter({
-            implements: IUserAdapter,
-            adapts: IStrong
-        })
-        
-        const UserPrototype = createObjectPrototype({
-            implements: [IUser, IStrong]
-        });
-        
-        const theUser = new UserPrototype();
-        
+
+    it('can get an adapter by specifying the adaptsInterface param', function () {
+        class IUser extends ObjectInterface {
+            get interfaceId() { return id('IUser') };
+        }
+
+        class IStrong extends ObjectInterface {
+            get interfaceId() { return id('IStrong') };
+        }
+
+        class IUserAdapter extends AdapterInterface {
+            get interfaceId() { return id('IUserAdapter') };
+        }
+        class UserAdapter extends Adapter {
+            get __implements__() { return IUserAdapter };
+            constructor({ adapts, Component, registry }: TAdapter) {
+                super({ adapts, Component, registry });
+            }
+        }
+
+        new UserAdapter({ adapts: IStrong })
+
+        class User extends ObjectPrototype<any> {
+            __implements__ = [IUser, IStrong];
+          }
+
+        const theUser = new User();
+
         const ua = registry.getAdapter(theUser, IUserAdapter, IStrong);
 
-        expect(ua).toBeInstanceOf(StrongAdapter);
-    });
-    
-    it('can get an adapter registered by object', function() {
-        const UserPrototype = createObjectPrototype({});
-        
-        const IUserAdapter = new Interface({name: "IUserAdapter"});
-        
-        const UserAdapter = new Adapter({
-            implements: IUserAdapter,
-            adapts: UserPrototype
-        })
-        
-        const theUser = new UserPrototype();
-        
-        const ua = new IUserAdapter(theUser);
-        
         expect(ua).toBeInstanceOf(UserAdapter);
     });
-    
-    
-    it('can get an adapter registered by interface by providing interface', function() {
-        const IUser = new Interface({name: "IUser"});
-        
-        const IUserAdapter = new Interface({name: "IUserAdapter"});
-        
-        const UserAdapter = new Adapter({
-            implements: IUserAdapter,
-            adapts: IUser
-        })
-        
+
+    it('can get an adapter registered by object', function () {
+        class User extends ObjectPrototype<any> {
+
+          }
+
+        class IUserAdapter extends AdapterInterface {
+            get interfaceId() { return id('IUserAdapter') };
+        }
+
+        class UserAdapter extends Adapter {
+            get __implements__() { return IUserAdapter };
+            constructor({ adapts, Component, registry }: TAdapter) {
+                super({ adapts, Component, registry });
+            }
+        }
+        new UserAdapter({ adapts: User })
+
+        const theUser = new User();
+
+        const ua = new IUserAdapter(theUser);
+
+        expect(ua).toBeInstanceOf(UserAdapter);
+    });
+
+
+    it('can get an adapter registered by interface by providing interface', function () {
+        class IUser extends ObjectInterface {
+            get interfaceId() { return id('IUser') };
+        }
+
+        class IUserAdapter extends AdapterInterface {
+            get interfaceId() { return id('IUserAdapter') };
+        }
+
+        class UserAdapter extends Adapter {
+            get __implements__() { return IUserAdapter };
+            constructor({ adapts, Component, registry }: TAdapter) {
+                super({ adapts, Component, registry });
+            }
+        }
+        new UserAdapter({ adapts: IUser })
+
         // TODO: This is pretty useless, should we support it?
         const ua = registry.getAdapter(IUser, IUserAdapter);
 
         expect(ua).toBeInstanceOf(UserAdapter);
     });
-    
-    it('can get an adapter for specific prototype', function() {
-        const IBase = new Interface({name: 'IBase'});
-        
-        const BasePrototype = createObjectPrototype({
-            implements: [IBase],
-        });
-        
-        const IListWidget = new Interface({name: 'IListWidget'});
-        
-        const BaseListWidget = new Adapter({
-            implements: IListWidget,
-            adapts: IBase
-        })
-        
-        const IUser = new Interface({name: 'IUser'});
-        
-        const UserPrototype = createObjectPrototype({
-            extends: [BasePrototype],
-            implements: [IUser]
-        });
-        
-        const UserListWidget = new Adapter({
-            implements: IListWidget,
-            adapts: IUser
-        })
-        
-        const theUser = new UserPrototype();
-        
-        const listWidget = new IListWidget(theUser);
-
-        expect(listWidget).toBeInstanceOf(UserListWidget);
-        expect(listWidget).not.toBeInstanceOf(BaseListWidget);
-    });
 });
 
-describe('Local Registry', function() {
+describe('Local Registry', function () {
 
-  it('can be created', function() {
-      const registry = new LocalRegistry()
-      expect(registry).not.toBe(undefined);
-  });
-
-  it('can get an unnamed utility', function() {
-    const registry = new LocalRegistry()
-    const IDummyUtility = new Interface({name: "IDummyUtility"});
-    
-    const DummyUtility = new Utility({
-        registry,
-        implements: IDummyUtility
-    });
-    
-    const util = new IDummyUtility({ registry });
-    
-    expect(util).toBeInstanceOf(DummyUtility);
-  });
-
-  it('no leaking to second registry', function() {
-    const localRegistry = new LocalRegistry()
-    const IDummyUtility = new Interface({name: "IDummyUtility"});
-    
-    const DummyUtility = new Utility({
-        registry: localRegistry,
-        implements: IDummyUtility
+    it('can be created', function () {
+        const registry = new LocalRegistry()
+        expect(registry).not.toBe(undefined);
     });
 
-    const localRegistry2 = new LocalRegistry()
-    
-    const util = new IDummyUtility({ registry: localRegistry });
-    const util2 = localRegistry2.getUtilities(IDummyUtility);
-    
-    expect(util).toBeInstanceOf(DummyUtility);
-    expect(util2.length).toEqual(0);
-  });
+    it('can get an unnamed utility', function () {
+        const registry = new LocalRegistry()
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
 
-  it('no leaking to global registry', function() {
-    const localRegistry = new LocalRegistry()
-    const IDummyUtility = new Interface({name: "IDummyUtility"});
-    
-    const DummyUtility = new Utility({
-        registry: localRegistry,
-        implements: IDummyUtility
+        const DummyUtility = new Utility({
+            registry,
+            implements: IDummyUtility
+        });
+
+        const util = new IDummyUtility(registry);
+
+        expect(util).toBeInstanceOf(DummyUtility);
     });
-    
-    const util = new IDummyUtility({ registry: localRegistry });
-    const util2 = registry.getUtilities(IDummyUtility);
-    
-    expect(util).toBeInstanceOf(DummyUtility);
-    expect(util2.length).toEqual(0);
-  });
+
+    it('no leaking to second registry', function () {
+        const localRegistry = new LocalRegistry()
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
+
+        const DummyUtility = new Utility({
+            registry: localRegistry,
+            implements: IDummyUtility
+        });
+
+        const localRegistry2 = new LocalRegistry()
+
+        const util = new IDummyUtility(localRegistry);
+        const util2 = localRegistry2.getUtilities(IDummyUtility);
+
+        expect(util).toBeInstanceOf(DummyUtility);
+        expect(util2.length).toEqual(0);
+    });
+
+    it('no leaking to global registry', function () {
+        const localRegistry = new LocalRegistry()
+        class IDummyUtility extends UtilityInterface {
+            get interfaceId() { return id('IDummyUtility') };
+        }
+
+        const DummyUtility = new Utility({
+            registry: localRegistry,
+            implements: IDummyUtility
+        });
+
+        const util = new IDummyUtility(localRegistry);
+        const util2 = registry.getUtilities(IDummyUtility);
+
+        expect(util).toBeInstanceOf(DummyUtility);
+        expect(util2.length).toEqual(0);
+    });
 });
